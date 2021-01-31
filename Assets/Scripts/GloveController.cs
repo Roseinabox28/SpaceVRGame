66dc474695f28d6294e4ca45e39559d3a64d871e
Rose <48871187+Roseinabox28@users.noreply.github.com>
2021-01-30 21:47:44 -0800

    add ArtifactDB

---
+++ Assets/Scripts/GloveController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GloveController : MonoBehaviour
+{
+    public Animator controller;
+    [Range(0,1)]
+    public float grip;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OVRInput.Update();
+        grip = OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger);
+    }
+    void FixedUpdate()
+    {
+        OVRInput.FixedUpdate();
+        grip = OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger);
+        controller.SetFloat("Grip", grip);
+        print("" + OVRInput.GetConnectedControllers());
+    }
+    public void SetGrip(float ngrip)
+    {
+        grip = ngrip;
+    }
+        
+    
+
+    
+}
---
+++ Assets/Scripts/GloveLController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GloveLController : MonoBehaviour
+{
+    public Animator controller;
+    [Range(0,1)]
+    public float grip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+     void Update()
+    {
+        OVRInput.Update();
+        grip = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger);
+    }
+    void FixedUpdate()
+    {
+        OVRInput.FixedUpdate();
+        grip = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger);
+        controller.SetFloat("Grip", grip);
+        print("" + OVRInput.GetConnectedControllers());
+    }
+}
